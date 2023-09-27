b64d11bb65694c3fa1be9aea4162b808fa58f066
Diana7119 <123771113+Diana7119@users.noreply.github.com>
2023-09-27 21:24:46 +0200

    1
    1

---
+++ src/components/App.styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Section = styled.section`
+display: flex;
+flex-direction: column;
+justify-content: center; 
+align-items: center; 
+padding: 34px;
+}
+`;
+
+export const MainHeader = styled.h1`
+  margin-bottom: 20px;
+`;
+
+export const SecondaryHeader = styled.h2`
+  margin-bottom: 20px;
+`;
